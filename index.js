929f25dcbe17fd04337bcc01404bbf0250efc62c
root <admin@example.com>
2022-09-02 15:04:22 +0300

    try auth

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const axios = require('axios');
-
 const cors = require('cors');
 app.use(cors())
 
@@ -10,7 +9,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!!!!!')
 })
 
-app.get("/api/nonces", async (req, res) => {
+app.get("/api/products", async (req, res) => {
   try {
       const response = await axios.get("https://api.printful.com/products")
       res.json(response.data)
@@ -20,11 +19,34 @@ app.get("/api/nonces", async (req, res) => {
   }
 })
 
+app.get("/api/nonces", async (req, res) => {
+  try {
+    const token = 'xaAg8OBVXFK2f6iynNmkktVorMxyK8MyCJys2xOS';
+    const response = await axios.post("https://api.printful.com/embedded-designer/nonces", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }, 
+        body: JSON.stringify({
+            "external_product_id": "307"
+          })
+        }
+    )
+    res.json(response.data)
+  }
+  catch (err) {
+      console.log(err)
+  }
+})
+
+
+
+
+
 app.get('*', (req, res) => {
   res.status(500).json({ message: "error" })
 })
 
 app.listen(port);
-
-// Export the Express API
 module.exports = app;
