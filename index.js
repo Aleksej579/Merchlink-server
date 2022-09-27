c35a82cd092790eb5a6e9a7e303dd064fe100f9a
root <admin@example.com>
2022-09-27 23:02:48 +0300

    test

--- index.js
+++ index.js
@@ -148,10 +148,10 @@ app.get('/api/orderprintful', function(req, res) {
 
 // get & send data to metafields customer or shop
 let metafieldBody;
-app.post('/api/sendmetafield', async function(req, res) {
+app.post('/api/sendmetafield', function(req, res) {
   try {
     const headers = {
-      'X-Shopify-Access-Token': `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`,
+      'X-Shopify-Access-Token': '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',
       'Content-Type': 'application/json'
     };
     const customerId = req.body.metafield.namespace;
@@ -159,8 +159,9 @@ app.post('/api/sendmetafield', async function(req, res) {
     const body = {
       "metafield": {
         "namespace": "customer_id",
-        "key": "default_collectionName",
-        "value": metaValue
+        "key": "collection_name",
+        "value": metaValue,
+        "type": "single_line_text_field"
       }
     };
     axios.post(`https://all-u-sportswear.myshopify.com/admin/api/2022-07/customers/${customerId}/metafields.json`, body, { headers });
