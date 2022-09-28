7210d17bf6f1c893124a08102f6068eb0efc7d90
root <admin@example.com>
2022-09-28 19:16:24 +0300

    hj

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.get('/api/orderprintful', function(req, res) {
     res.json(arrOrder);
 });
 
-// get & send data to metafields customer or shop
+// get & send data to metafields customer
 let metafieldBody;
 app.post('/api/sendmetafield', async function(req, res) {
   try {
@@ -162,15 +162,16 @@ app.post('/api/sendmetafield', async function(req, res) {
         // } else {
         //   metafieldBody = `${req.body.metafield.value},${response.data.metafields[0].value}`;
         // }
-        metafieldBody = `${req.body.metafield.value},${response.data.metafields[0].value}`;
+        // metafieldBody = `${req.body.metafield.value},${response.data.metafields[0].value}`;
 
-        // metafieldBody = `${req.body.metafield.value},${response.data.metafields[0].value || null}`;
+        // metafieldBody = `${req.body.metafield.value},`;
 
         const headers = {
           'X-Shopify-Access-Token': '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',
           'Content-Type': 'application/json'
         };
-        const metaValue = metafieldBody;
+        // const metaValue = metafieldBody;
+        const metaValue = `${req.body.metafield.value},`;
         const body = {
           "metafield": {
             "namespace": "customer_id",
