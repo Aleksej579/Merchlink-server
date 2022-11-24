f19a443ff1de5a6a75a4184a89bbe8fd400f48b2
Aleksej579 <aleksej.eremenko579@gmail.com>
2022-11-24 09:52:37 +0200

    test

--- index.js
+++ index.js
@@ -206,22 +206,25 @@ app.post('/api/changemetafield', function(req, res) {
           "type": "single_line_text_field"
         }
       };
-      axios.post(`https://all-u-sportswear.myshopify.com/admin/api/2022-07/customers/${customerId}/metafields.json`, body, { headers });
+      axios.post(`https://all-u-sportswear.myshopify.com/admin/api/2022-07/customers/${customerId}/metafields.json`, body, { headers })
+      .then((response) => {
+        // res.json(response.data);
 
-      const headers_ = {
-        'X-Shopify-Access-Token': process.env.ACCESS_TOKEN_SHOPIFY,
-        'Content-Type': 'application/json'
-      };
-      const body_ = {
-        "metafield": {
-          "namespace": `${customerId}`,
-          "key": `${customerId}`,
-          "value": `${newData}`,
-          "type": "single_line_text_field"
-        }
-      };
-      axios.post('https://all-u-sportswear.myshopify.com/admin/api/2022-10/metafields.json', body_, { headers_ });
-
+        const headers_ = {
+          'X-Shopify-Access-Token': process.env.ACCESS_TOKEN_SHOPIFY,
+          'Content-Type': 'application/json'
+        };
+        const body_ = {
+          "metafield": {
+            "namespace": `${customerId}`,
+            "key": `${customerId}`,
+            "value": `${newData}`,
+            "type": "single_line_text_field"
+          }
+        };
+        axios.post('https://all-u-sportswear.myshopify.com/admin/api/2022-10/metafields.json', body_, { headers_ });
+        
+      });
     })
   }
   catch (err) {
