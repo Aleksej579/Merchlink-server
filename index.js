0d4986fd25e427eea2ff62d9a1e985bee62b2dcd
Aleksej579 <aleksej.eremenko579@gmail.com>
2022-12-28 09:04:50 +0200

    del.

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.get("/api/saveimagefromurl/:gtkey/:customer", (req, res) => {
       let arrLinkToImage = resp.data.result.mockups;
       arrLinkToImage.forEach((element, index) => {
         fetch(element.mockup_url).then(res => {
-          res.body.pipe(fs.createWriteStream(`./tmp/${customer}/${gt}/image-${index}.png`));
+          res.body.pipe(fs.createWriteStream(`/tmp/${customer}/${gt}/image-${index}.png`));
         });
       });
       res.send({
