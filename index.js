605823f4e43e21b76571e791d17ebfd1355d1b08
Aleksej579 <aleksej.eremenko579@gmail.com>
2023-01-24 22:44:04 +0200

    test1

--- index.js
+++ index.js
@@ -174,9 +174,8 @@ app.get('/api/image/:prodId', function(req, res) {
 let arrBody = [];
 app.post('/api/orderprintful', async function(req, res) {
   const arrProperty = [];
-
   for(let [index, item] of req.body.line_items.entries()) {
-    const keyGt = item.properties[0]?.value;
+    const keyGt = item.properties[0].value;
     arrProperty.push(item.properties[0]?.value);
     try {
       await axios.get(`https://api.printful.com/mockup-generator/task?task_key=${keyGt}`, {
