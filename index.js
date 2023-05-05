f00ba3f891897480774c05a6e9da27fd95b079ac
Aleksej579 <aleksej.eremenko579@gmail.com>
2023-05-05 15:33:28 +0300

    wsw

--- index.js
+++ index.js
@@ -205,7 +205,11 @@ app.get("/api/template/:templateId/:external_product_id", async (req, res) => {
 app.get('/api/image/:prodId', (req, res) => {
   if (req.params.prodId) {
 
-    if (axios.get(`https://api.printful.com/product-templates/@${req.params.prodId}`, {headers: {Authorization: `Bearer ${process.env.TOKEN_PRINTFUL}`, 'X-PF-Store-ID': process.env.STORE_ID }}).status === 200) {
+    if (axios.get(`https://api.printful.com/product-templates/@${req.params.prodId}`, 
+      {headers: {Authorization: `Bearer ${process.env.TOKEN_PRINTFUL}`, 
+      'X-PF-Store-ID': process.env.STORE_ID }
+      }
+    ).status == 404) {
 
       
       axios.get(`https://api.printful.com/product-templates/@${req.params.prodId}`, {headers: {Authorization: `Bearer ${process.env.TOKEN_PRINTFUL}`, 'X-PF-Store-ID': process.env.STORE_ID }})
